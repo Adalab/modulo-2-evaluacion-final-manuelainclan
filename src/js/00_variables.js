88c5663762a3a0e873ec3dc3caca095d14a28f76
manuelainclan <minclanfigueiras@gmail.com>
2023-02-24 10:08:30 +0100

    Add js variables & html inputs

---
+++ src/js/00_variables.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const searchInput = document.querySelector('.js-search-input');
+const searchBtn = document.querySelector('.js-search-btn');
+const resetBtn = document.querySelector('.js-reset-btn');
+const drinkList = document.querySelector('.js-drink-list');
